9aa6efa351b28eb80614bc5efa30c849407bd9bc
Sebastião Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2023-05-14 10:55:33 +0100

    add getter for newline to print node

--- ast/print_node.h
+++ ast/print_node.h
@@ -22,6 +22,10 @@ namespace mml {
       return _argument;
     }
 
+    inline bool newline() {
+      return _newline;
+    }
+
     void accept(basic_ast_visitor *sp, int level) {
       sp->do_print_node(this, level);
     }
