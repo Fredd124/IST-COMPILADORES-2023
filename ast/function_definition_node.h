7022dd4859d255eed5ff0e6b8129f24dafd88120
Frederico Silva <frederico.m.silva@tecnico.ulisboa.pt>
2023-05-19 12:09:57 +0100

    fix: fixed function definition node

--- ast/function_definition_node.h
+++ ast/function_definition_node.h
@@ -17,9 +17,9 @@ namespace mml {
 
   public:
     inline function_definition_node(int lineno, int access, const std::vector<std::shared_ptr<cdk::basic_type>> &inputTypes, 
-            const std::shared_ptr<cdk::basic_type> &outputType, const std::string &identifier,
-            cdk::sequence_node *parameters, mml::block_node *block, bool isMain == false) :
-        cdk::typed_node(lineno), _access(access), _identifier(identifier), _parameters(parameters), _block(block), _isMain(isMain) {
+            const std::shared_ptr<cdk::basic_type> &outputType,
+            cdk::sequence_node *parameters, mml::block_node *block, bool isMain = false) :
+        cdk::expression_node(lineno), _access(access), _parameters(parameters), _block(block), _isMain(isMain) {
         std::shared_ptr<cdk::functional_type> functionalType = _functionalType->create(inputTypes, outputType);
         type(functionalType);
     }
